f0155466d4fdb0b9798a4d5e73d3536f8f2b8408
SihwanGit <sihwan1214@naver.com>
2026-01-10 22:53:13 +0900

    Crafting Interpreters 4.1장 : Error&Report 추가

--- src/com/craftinginterpreters/lox/Lox.java
+++ src/com/craftinginterpreters/lox/Lox.java
@@ -9,6 +9,8 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    static boolean hadError = false; //4.1 에러 여부 체크
+
     public static void main(String[] args) throws IOException {
         if(args.length > 1) { //4장. 입력된 문장이 1보다 크다면
             System.out.println("Usage : jlox [script]"); //실행 출력
@@ -23,7 +25,11 @@ public class Lox {
     //4장. Jlox를 기동할 떄 파일 경로를 지정해 스크립트 파일을 실행
     private static void runFile(String path) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(path)); //파일 읽기
-        run(new String(bytes, Charset.defaultCharset())); //charset에 넣은 뒤 실행
+        run(new String(bytes, Charset.defaultCharset()));//charset에 넣은 뒤 실행
+
+        //4.1장 종료 코드로 에러를 식별한다.
+        if(hadError) System.exit(65); //파일을 받고나서 에러가 있으면 종료.
+        //이 코드의 위치가 여기가 아닐 수 있는데 그건 나중에 수정하면 됨.
     }
 
     //4장. Jlox를 기동할 때 파일이 아닌 프롬프트로 직접 입력해 실행
@@ -37,6 +43,7 @@ public class Lox {
             String line = reader.readLine(); //사용자가 명령줄에 입력한 코드를 읽어 리턴
             if(line == null) break; //종료 조건. Crtl + D를 누르면 종료된다
             run(line); //코드를 읽어 한줄 씩 실행
+            hadError = false; //4.1장 에러가 발생한 줄만 종료시키기 위해 분리
         }
     }
 
@@ -50,6 +57,17 @@ public class Lox {
         for(Token token : tokens) {
             System.out.println(token);
         }
+    }
+
+    static void error(int line, String message) { //4.1장 error를 관리하는 함수
+        report(line, "", message); //line과 메세지를 report로 전달
+    }
+
+    //4.1장 에러가 발생한 줄을 알려주고, haddError 값을 true로 바꿔 코드를 종료한다.
+    //Lox는 에러가 발생한 라인만 표시한다.
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
     }
 
 }
