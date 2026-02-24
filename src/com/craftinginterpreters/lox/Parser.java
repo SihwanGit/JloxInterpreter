3635f89683a381f1b97135429d01bbdba76c68bb
SihwanGit <sihwan1214@naver.com>
2026-02-24 22:33:58 +0900

    6.2 : term

--- src/com/craftinginterpreters/lox/Parser.java
+++ src/com/craftinginterpreters/lox/Parser.java
@@ -86,13 +86,26 @@ public class Parser {
     private Expr comparison() {
         Expr expr = term();
 
-        while(match(GREATER_EQUAL, GREATER, LESS, LESS_EQUAL)) {
+        while (match(GREATER_EQUAL, GREATER, LESS, LESS_EQUAL)) {
             Token operator = previous();
             Expr right = term();
             expr = new Expr.Binary(expr, operator, right);
         }
 
         return expr;
+    } //realization is similar to equality
+
+    //6.2 항 : term -> factor ( ( "+" | "-" ) factor )*
+    private Expr term() {
+        Expr expr factor();
+
+        while(match(MINUS, PLUS)) {
+            Token operator = previous();
+            Expr right = factor();
+            expr = new Expr.Binary(expr, operator, right);
+        }
+        return expr;
     }
-    //realization is similar to equality
+
+
 }
